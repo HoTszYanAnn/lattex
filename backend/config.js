4843fefa008eff1e0c398bc151d2eb4543cdb8bc
HoTszYanAnn <tszyan.ann@gmail.com>
2021-04-14 20:01:46 +0800

    fix heroku redirect bug

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 exports.git = {
   client_id: '91ba4217f7895ed7e855',
   client_secret: 'e73d14425c267d170211ebc394656e82b785e412',
-  redirect_uri: 'http://localhost:3000'
+  redirect_uri: 'https://HoTszYanAnn.github.io/lattex'
 } 
